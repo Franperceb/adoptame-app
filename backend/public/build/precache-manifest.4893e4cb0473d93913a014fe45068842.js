2e2e9ff682474971f93fffe488f8d67980f04866
Francisco <franperceb@gmail.com>
2019-07-31 16:16:37 -0500

    second deploy

---
+++ backend/public/build/precache-manifest.4893e4cb0473d93913a014fe45068842.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2887abbcd7f4c0218e102f4e13e12a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7749f519f848502babef",
+    "url": "/static/css/main.9db8fde1.chunk.css"
+  },
+  {
+    "revision": "f6eb47a7381cf0579f56",
+    "url": "/static/js/2.44835c37.chunk.js"
+  },
+  {
+    "revision": "7749f519f848502babef",
+    "url": "/static/js/main.7cc61505.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6b74c6739f75e70272c5a6c44a31d870",
+    "url": "/static/media/DogLanding.6b74c673.png"
+  },
+  {
+    "revision": "1d7ce1c55b23a3807bfabe6cc55e2153",
+    "url": "/static/media/logo.1d7ce1c5.svg"
+  }
+]);
